723eaa2fce34dce28aa07a82d2e4fd602e1b21d0
Daohan Chong <dachon@microsoft.com>
2022-02-17 15:29:28 +0800

    Add rust test

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,57 +1,39 @@
+struct WikipediaExpectation {
+    title: String,
+    charset: String,
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::WikipediaExpectation;
     use kuchiki::traits::TendrilSink;
     use std::{borrow::Borrow, io::Read};
 
-    use crate::WikipediaExpectation;
-
     #[test]
-    fn it_works() {
+    fn run_test() {
         let html = load_wikipedia_html();
         let document = kuchiki::parse_html().one(html);
         let expectation = load_expectations();
+        let start_time = std::time::Instant::now();
 
-        let css_selector = "title";
+        // test 100,000 times
+        for _ in 0..100_000 {
+            let title_element = document.select("title").unwrap().next().unwrap();
+            let title_text = title_element.text_contents();
+            assert_eq!(title_text, expectation.title);
 
-        let title_element = document.select("title").unwrap().next().unwrap();
-        let title_text = title_element.text_contents();
-        assert_eq!(title_text, expectation.title);
-
-        let meta_element = document.select("meta[charset]").unwrap().next().unwrap();
-        let attributes = meta_element.attributes.borrow();
-        if let Some(meta_charset_attribute) = attributes.get("charset") {
-            assert_eq!(meta_charset_attribute, expectation.charset);
-        } else {
-            panic!("No charset attribute found");
+            let meta_element = document.select("meta[charset]").unwrap().next().unwrap();
+            let attributes = meta_element.attributes.borrow();
+            if let Some(meta_charset_attribute) = attributes.get("charset") {
+                assert_eq!(meta_charset_attribute, expectation.charset);
+            } else {
+                panic!("No charset attribute found");
+            }
         }
 
-        for css_match in document.select(css_selector).unwrap() {
-            // css_match is a NodeDataRef, but most of the interesting methods are
-            // on NodeRef. Let's get the underlying NodeRef.
-            let as_node = css_match.as_node();
+        let end_time = std::time::Instant::now();
 
-            // In this example, as_node represents an HTML node like
-            //
-            //   <p class='foo'>Hello world!</p>"
-            //
-            // Which is distinct from just 'Hello world!'. To get rid of that <p>
-            // tag, we're going to get each element's first child, which will be
-            // a "text" node.
-            //
-            // There are other kinds of nodes, of course. The possibilities are all
-            // listed in the `NodeData` enum in this crate.
-            let text_node = as_node.first_child().unwrap();
-
-            // Let's get the actual text in this text node. A text node wraps around
-            // a RefCell<String>, so we need to call borrow() to get a &str out.
-            let text = text_node.as_text().unwrap().borrow();
-
-            // Prints:
-            //
-            //  "Hello, world!"
-            //  "I love HTML"
-            println!("{:?}", text);
-        }
+        print!("Duration: {}", end_time.duration_since(start_time).as_millis());
     }
 
     fn load_wikipedia_html() -> String {
@@ -78,9 +60,4 @@ mod tests {
             charset: charset,
         };
     }
-}
-
-struct WikipediaExpectation {
-    title: String,
-    charset: String,
 }
